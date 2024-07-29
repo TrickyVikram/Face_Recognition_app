f68865a34c371bea983a1c48366ff522f362d6bd
TrickyVikram <vkumarsah999@gmail.com>
2024-07-30 04:37:07 +0530

    Update

--- EncdoeGenerator.py
+++ EncdoeGenerator.py
@@ -8,16 +8,18 @@ folderPath = "Images"
 if not os.path.exists(folderPath):
     print(f"Directory {folderPath} does not exist.")
 
-
 pathList = os.listdir(folderPath)
 imgList = []
-
-print(pathList)
-
 StudentsId = []
+
 # Read each image and append it to the list
 for path in pathList:
     img = cv2.imread(os.path.join(folderPath, path))
     imgList.append(img)
 
-print(len(imgList))
+    # Get the student's id
+    StudentsId.append(os.path.splitext(path)[0])
+
+print(StudentsId)
+
+# Encode the images
