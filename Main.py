7d23f9caa3dcead6ff86e71c7c6228c5f343a1e6
TrickyVikram <vkumarsah999@gmail.com>
2024-07-30 03:39:58 +0530

    Update

--- Main.py
+++ Main.py
@@ -1,37 +1,51 @@
 import cv2
 import os
 
+# Initialize webcam
 cap = cv2.VideoCapture(0)
-cap.set(3, 640)#width
-cap.set(4, 480)#height
+cap.set(3, 640) # set width
+cap.set(4, 480) # set height
 
+# Load the background image
 img_background = cv2.imread("./Resources/Background.png")
 
-# Load the images list from the "Modes" folder
+# Load the images from the "Modes" folder
 folderModesPath = "./Resources/Modes/"
 modepathList = os.listdir(folderModesPath)
 imgModesList = []
 
 # Read each image and append it to the list
 for path in modepathList:
-    imgModesList.append(cv2.imread(f"{folderModesPath}{path}"))
+    img = cv2.imread(f"{folderModesPath}{path}")
+    imgModesList.append(img)
 
-
-
+# Resize the mode image to be used
+imgModesList_Shape = cv2.resize(imgModesList[0], (482, 798))
 
 while True:
-    sucess, img = cap.read()
- 
- 
-
-    img_background[376:376+480, 220:220+640]=img
-
-    img_background[10:10+50, 500:500+50]=imgModesList[1]
-   
+    success, img = cap.read()
+    
 
 
-    cv2.imshow("Face Attendance", img_background)
-    cv2.waitKey(1)
 
+
+
+    
    
     
+    # Place the webcam feed on the background
+    img_background[376:376+480, 220:220+640] = img
+
+    # Place the resized mode image on the background
+    img_background[152:152+798, 1238:1238+482] = imgModesList_Shape
+
+    # Display the result
+    cv2.imshow("Face Attendance", img_background)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
